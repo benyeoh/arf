e6d7be29ef148d84d8628fbc5fc32df6c03b5ff9
BenAtWork <10702317+benyeoh@users.noreply.github.com>
2013-08-08 20:42:47 +0900

    Add work intersection code

---
+++ Temp/LmOrthoFrustum_PC.cpp
@@ -0,0 +1,22 @@
+/**
+ *  @file       LmOrthoFrustum_PC.cpp
+ *  @brief
+ *
+ *	@date       2013/8/1
+ *	@author     Ben Yeoh
+ *
+ */
+
+#include "LmPrecompile.h"
+#include "Math/LmVector3.h"
+#include "Math/LmAABB.h"
+#include "Math/LmOrientedBB.h"
+#include "Math/LmSphere.h"
+#include "Math/LmMatrix.h"
+#include "Math/LmMatrix4.h"
+#include "Math/LmOrthoFrustum.h"
+
+LM_NS_BEGIN_MATH
+
+
+LM_NS_END_MATH
